41d122b748b823e344efedd5a804da0c3756a4a7
civecat <609176664@qq.com>
2022-05-15 22:32:01 +0800

    第二次提交-简单完成侧栏菜单

--- vue-manage/views/Home.vue
+++ vue-manage/views/Home.vue
@@ -1,12 +1,23 @@
 <template>
-    <div>
-        我是Home1页面
-    </div>
+<el-container style="height: 100%;">
+  <el-aside width="auto">
+    <common-aside></common-aside>
+  </el-aside>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
+import CommonAside from '../src/components/CommonAside.vue'
+
 export default{
     name:'vueHome',
+    components:{
+      CommonAside
+    },
     data() {
         return {
             
@@ -14,3 +25,11 @@ export default{
     }
 }
 </script>
+<style lang="less">
+.el-header{
+    background: #333;
+}
+.el-main{
+    padding-top: 0;
+}
+</style>
